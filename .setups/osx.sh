6b8ae4912ab499a5e8a6a5d474de02ed56f50c32
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2020-09-16 19:10:55 +0900

    Tweaks Chrome defaults

--- .setups/osx.sh
+++ .setups/osx.sh
@@ -755,15 +755,12 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 
 # Use the system-native print preview dialog
 defaults write com.google.Chrome DisablePrintPreview -bool true
-defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
-defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
 
-# Put Disk Cache Size (2GB)
-defaults write com.google.Chrome DiskCacheSize -integer 2147483648
-defaults write com.google.Chrome.canary DiskCacheSize -integer 2147483648
+# Put Disk Cache Size (100MB)
+defaults write com.google.Chrome DiskCacheSize -integer 104857600
 
 
 ###############################################################################
