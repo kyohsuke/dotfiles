c596f3dcda99aad660399a63e38547805b111702
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2021-05-24 04:00:48 +0900

    Add whalebrew

--- .setups/brew/whalebrew.sh
+++ .setups/brew/whalebrew.sh
@@ -4,7 +4,14 @@ if test ! "$(command -v whalebrew)"; then
   if [ "$WORK" != "true" ]; then
     brew install whalebrew
 
-    whalebrew whalebrew/imagemagick
-    whalebrew whalebrew/youtube-dl
+    WHALEBREWS=(
+      imagemagick
+      ffmpeg
+    )
+
+    for WHALEBREW in "${WHALEBREWS[@]}"
+    do
+      whalebrew "whalebrew/${WHALEBREW}"
+    done
   fi
 fi
