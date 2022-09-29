80bc06a2fe47e1e0b5790f030456262dff4369c7
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-09-29 14:18:36 +0900

    Tweaks setup fish

--- .setups/fish.sh
+++ .setups/fish.sh
@@ -4,7 +4,7 @@ if [ "$SHELL" != "/usr/local/bin/fish" ]; then
   # Install Fish
   brew install fish fisher
 
-  fish -C "source /usr/local/share/fish/vendor_functions.d/fisher.fish; fisher update; exit"
+  fish -C "source /usr/local/share/fish/vendor_functions.d/fisher.fish && fisher update; exit"
   
   # Change to the new shell, prompts for password
   sudo chsh -s /usr/local/bin/fish "$USER"
