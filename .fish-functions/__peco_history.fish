3151aea4005c49bfdad4f484deeeba70c5e65795
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-09-29 14:18:20 +0900

    Fixes peco_history

--- .fish-functions/__peco_history.fish
+++ .fish-functions/__peco_history.fish
@@ -1,6 +1,6 @@
 function __peco_history
   commandline | read -l buffer
-  history | tail -r  | awk '!a[$0]++' | peco --query "$buffer" | read -l cmd
+  history | awk '!a[$0]++' | peco --query "$buffer" | read -l cmd
   if test -n "$cmd"
     commandline "$cmd"
   end
