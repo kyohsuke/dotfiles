0f660ae2248af27ee8ade0fd7b1b808736dc7777
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2022-12-02 00:02:23 +0900

    Tweaks gobin-update

--- .fish-functions/gobin-update.fish
+++ .fish-functions/gobin-update.fish
@@ -5,12 +5,22 @@
 # See Also: https://zenn.dev/kyoh86/articles/291618538dcf0d
 function gobin-update
   set -f gobin "$(go env GOBIN)"
+  set -f currentVersion "$(go version | awk '{print $3}')"
+  echo "[INFO] current golang version is $currentVersion"
+  echo "[INFO] check for build versions..."
 
   for file in "$gobin"/*
-    set -f pkg "$(go version -m "$file" 2> /dev/null | head -n2 | tail -n1 | awk '{print $2}')"
-    if test -n "$pkg"
+    set -f temp "$(go version -m "$file" 2> /dev/null)"
+    set -f pkg "$(echo "$temp" | head -n2 | tail -n1 | awk '{print $2}')"
+    set -f binVersion "$(echo "$temp" | head -n1 | awk '{print $2}')"
+    set -f binName "$(basename $pkg)"
+
+    if test -n "$pkg" && test "$currentVersion" != "$binVersion"
+      echo "[INFO] rebuild $(basename $pkg) from $binVersion to $currentVersion."
       go install "$pkg@latest"
     end
   end
+
+  echo "[INFO] finished."
 end
 
