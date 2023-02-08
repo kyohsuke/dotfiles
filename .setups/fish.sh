e434c07a35430315ae82a5994a807133e2b928ac
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2023-02-08 15:14:51 +0900

    Tweaks install script  for fish

--- .setups/fish.sh
+++ .setups/fish.sh
@@ -5,7 +5,11 @@ if [ "$SHELL" != "/usr/local/bin/fish" ]; then
   brew install fish fisher
 
   fish -C "source /usr/local/share/fish/vendor_functions.d/fisher.fish && fisher update; exit"
-  
+
+  # Install oh-my-fish
+  curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+  fish -C "omf update && omf install coffeeandcode && omf theme coffeeandcode"
+
   # Change to the new shell, prompts for password
   sudo chsh -s /usr/local/bin/fish "$USER"
 fi
