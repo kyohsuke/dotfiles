95bb69f2d76e79306fcfa6dedea18c5986045da5
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2024-04-02 22:52:03 +0900

    Tweaks

--- .fish-functions/gobin-update.fish
+++ .fish-functions/gobin-update.fish
@@ -1,18 +1,24 @@
 # See Also: https://zenn.dev/kyoh86/articles/291618538dcf0d
 function gobin-update
-    set -f gobin "$(go env GOBIN)"
-    set -f currentVersion "$(go version | awk '{print $3}')"
+    set -l gobin "$(go env GOBIN)"
+    set -l currentVersion "$(go version | awk '{print $3}')"
     echo "[INFO] current golang version is $currentVersion"
     echo "[INFO] check for build versions..."
 
     for fname in "$gobin"/*
-        set -f temp "$(go version -m "$fname" 2> /dev/null)"
-        set -f pkg "$(echo "$temp" | head -n2 | tail -n1 | awk '{print $2}')"
-        set -f binVersion "$(echo "$temp" | head -n1 | awk '{print $2}')"
-        set -f binName "$(basename $pkg)"
+        set -l temp (go version -m "$fname" 2> /dev/null)
+        set -l pkg (echo "$temp" | grep path | awk '{print $2}')
+        set -l binVersion (echo "$temp" | head -n1 | awk '{print $2}')
+        set -l binName (basename $fname)
+
+        if test -z "$binVersion"
+            echo "[INFO] $binName is not a go binary"
+            continue
+        end
 
         if test -n "$pkg" && test "$currentVersion" != "$binVersion"
             echo "[INFO] rebuild $binName from $binVersion to $currentVersion."
+            echo "[INFO] go install $pkg@latest"
             go install "$pkg@latest"
         else
             echo "[INFO] $binName is up to date."
