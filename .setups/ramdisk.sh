4c9d4a5cddd13c83ffa77031595b8e5dd1ce7e38
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2024-04-21 20:59:41 +0900

    Add ramdisk.sh

---
+++ .setups/ramdisk.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+DISK_SIZE=4194304
+DISK_NAME=RAMDisk
+
+MOUNT_VOLUME="$(hdiutil attach -nomount ram://${DISK_SIZE} | tr -d '[:space:]')"
+
+# create and mount RAMDisk
+# HFS+
+echo "diskutil erasevolume HFS+ ${DISK_NAME} \"${MOUNT_VOLUME}\""
+diskutil erasevolume HFS+ ${DISK_NAME} "${MOUNT_VOLUME}" || exit 1
+# APFS
+# diskutil partitionDisk `hdiutil attach -nomount ram://${DISK_SIZE}` 1 apfs ${DISK_NAME} ${DISK_SIZE}
+
+# create links to RAMDisk
+function symbolic_link(){
+  rm -rf "$1"
+  rm -rf "$2"
+  mkdir -p "$2"
+  ln -s "$2" "$1"
+}
+
+Chrome_Cache
+SRC="${HOME}/Library/Caches/Google/Chrome"
+DST="/Volumes/${DISK_NAME}/Caches/Chrome"
+symbolic_link "${SRC}" "${DST}"
