1e9f8e65ea07f6c077cb90bf176262afb3238577
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2024-12-20 01:26:49 +0900

    Tweaks for Apple M1

--- .setups/fish.sh
+++ .setups/fish.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$SHELL" != "/usr/local/bin/fish" ]; then
+FISH_PATH="$(command -v fish)"
+if [ "$SHELL" != "$FISH_PATH" ]; then
   # Install Fish
   brew install fish fisher
 
-  fish -C "source /usr/local/share/fish/vendor_functions.d/fisher.fish && fisher update; exit"
+  fish -C "source $HOMEBREW_PREFIX/share/fish/vendor_functions.d/fisher.fish && fisher update; exit"
 
   # Install oh-my-fish
   curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
   fish -C "omf update && omf install coffeeandcode && omf theme coffeeandcode"
 
   # Change to the new shell, prompts for password
-  echo "/usr/local/bin/fish" >>/etc/shells
-  sudo chsh -s /usr/local/bin/fish "$USER"
+  echo "$FISH_PATH" >>/etc/shells
+  sudo chsh -s "$FISH_PATH" "$USER"
 fi
