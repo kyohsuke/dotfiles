5745da7a747302c48d516759eb04d5cbe2801963
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2025-01-19 10:57:36 +0900

    Tweaks zpack

--- .fish-functions/zpack.fish
+++ .fish-functions/zpack.fish
@@ -1,16 +1,27 @@
 function zpack -a dirName
+    function _usage
+        echo "[Usage] zpack <DirName>"
+        exit 1
+    end
+
     if command -vq zpaq
+        if test "" = "$dirName"
+            _usage
+        end
+
         set -f baseName (basename $dirName)
         echo "$baseName"
 
         if test "." = "$baseName"
-            echo "[Usage] zpack <DirName>"
-            return 1
+            _usage
         end
+
         zpaq a "$baseName.zpaq" "$baseName" -m5 -not "*/.DS_Store"
         return $status
     else
-        echo "[Error] zpaq not installed."
-        return 1
+        echo "[Error] zpaq not installed"
+        echo "[Info] if you installed homebrew, use `brew install zpaq`"
+        exit 1
     end
 end
+
