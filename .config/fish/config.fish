c5076e74e18be38fb9229ad0e015c82a9d92af80
KAWAHARA Keisuke <kyohsuke@users.noreply.github.com>
2026-07-05 17:00:11 +0900

    Tweaks fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,21 @@
-test -f "$HOME/.config/fish/env"
-and source "$HOME/.config/fish/env"
-
-status --is-login
-and test -f "$HOME/.config/fish/profile"
-and source "$HOME/.config/fish/profile"
-
-if status --is-login || status --is-interactive
-  test -f "$HOME/.config/fish/rc"
-  and source "$HOME/.config/fish/rc"
+if test -f "$HOME/.config/fish/env"
+    source "$HOME/.config/fish/env"
 end
 
-status --is-login
-and not test -f "$HOME/.config/fish/profile"
-and test -f "$HOME/.config/fish/login"
-and source "$HOME/.config/fish/login"
+if status --is-login; and test -f "$HOME/.config/fish/profile"
+    source "$HOME/.config/fish/profile"
+end
+
+if begin; status --is-login; or status --is-interactive; end; and test -f "$HOME/.config/fish/rc"
+    source "$HOME/.config/fish/rc"
+end
+
+if status --is-login; and not test -f "$HOME/.config/fish/profile"; and test -f "$HOME/.config/fish/login"
+    source "$HOME/.config/fish/login"
+end
+
+# {{{ Change Key Bindings if its not vim terminal
+if set -q VIM_TERMINAL
+    fish_default_key_bindings
+end
+# }}}
